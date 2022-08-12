1eedbfe216fec339e7c91e0f689d374d02499446
haimrubin <73288124+haimrubin@users.noreply.github.com>
2022-08-12 14:56:55 +0300

    .

--- src/component/About/Images.js
+++ src/component/About/Images.js
@@ -1,5 +1,5 @@
 import img1 from "../../photos/img1.jpeg";
-import img2 from "../../photos/img2.jpeg";
+import img22 from "../../photos/img22.jpeg";
 import img3 from "../../photos/img3.jpeg";
 import img4 from "../../photos/img4.jpeg";
 import img5 from "../../photos/img5.jpeg";
@@ -7,7 +7,7 @@ import Card from "../../UI/Card";
 
 import classes from "./Images.module.css";
 
-const images = [img3, img2, img4, img1, img5];
+const images = [img3, img22, img4, img1, img5];
 const imgList = images.map((item) => (
   <img src={item} height={500} width={"19%"} alt="images"></img>
 ));
